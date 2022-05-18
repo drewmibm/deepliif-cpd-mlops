b624afc08f4ba40761235ce98b8b4adbcb5eea67
Wendy Wang <wanting.wang@ibm.com>
2022-05-18 17:02:34 -0400

    moved mlops doc

--- credential_helper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export GIT_USER="enter your git username here"
-
-
-if [[ "$GIT_USER" == *enter* ]]; then
-    echo "Please configure credential_helper.sh with your git username."
-    exit 0
-fi
-git config --global credential.helper '!f() { sleep 1; echo "username='$GIT_USER'"; echo "password=${GIT_ACCESS_TOKEN}"; }; f'
